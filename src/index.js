c1506383e58cec2f3d5852ea12f91c50464eb4b5
livotieno <livio5261@gmail.com>
2023-11-03 10:25:43 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import About from './About';
+import Login from "./Login";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>About
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
